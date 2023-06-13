63ab8437c4b2eae3d2bafa21fe289998aa8c6188
Philippe 'paHpa' Vivien <pahpa@orange.fr>
2023-06-13 17:58:29 +0200

    WIP

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from ..gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
@@ -9,8 +9,4 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
-
-        
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual("foo", items[0].name)
